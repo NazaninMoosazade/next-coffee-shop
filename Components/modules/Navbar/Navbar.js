8aaf9db8d034f76de410d82c62175b47aafcada7
Nazanin Moosazade <moosazadenazanin@gmail.com>
2025-08-08 15:43:01 +0330

    coding searchHandler

--- Components/modules/Navbar/Navbar.js
+++ Components/modules/Navbar/Navbar.js
@@ -1,18 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "@/styles/Navbar.module.css";
 import Link from "next/link";
 
-export default function Navbar() {
+// Fontawesome
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { useRouter } from "next/router";
+
+function Navbar() {
+  const route = useRouter();
+  const [search, setSearch] = useState("");
+
+  const searchHandlerWithEnter = (event) => {
+    if (event.keyCode === 13) {
+      if (search.trim()) {
+        route.push(`/search?q=${search}`);
+      }
+    }
+  };
+
+  const searchHandler = () => {
+    if (search.trim()) {
+      route.push(`/search?q=${search}`);
+    }
+  };
+
   return (
     <div className={`container-fluid p-0 ${styles.nav_bar}`}>
       <nav
         className={`${styles.navbar} ${styles.navbar_expand_lg} bg-none navbar-dark py-3`}
       >
-        <a href="index.html" className={`${styles.navbar_brand} px-lg-4 m-0`}>
-          <h1 className="m-0 display-4 text-uppercase text-white">
-            Next-Coffee
-          </h1>
-        </a>
+        <div className="d-flex align-items-center position-relative">
+          <Link href="/" className={`${styles.navbar_brand} px-lg-4 m-0`}>
+            <h1 className="m-0 display-4 text-uppercase text-white">
+              Next-Coffee
+            </h1>
+          </Link>
+
+          <input
+            value={search}
+            onChange={(event) => setSearch(event.target.value)}
+            onKeyDown={searchHandlerWithEnter}
+            type="text"
+            className={styles.search_input}
+            placeholder="Search..."
+          />
+          <FontAwesomeIcon
+            onClick={searchHandler}
+            className={styles.search_icon}
+            icon={faSearch}
+          />
+        </div>
         <button
           type="button"
           className={`${styles.navbar_toggler}`}
@@ -32,24 +73,29 @@ export default function Navbar() {
             >
               Home
             </Link>
+
             <Link href="/about" className={`${styles.nav_link}`}>
               About
             </Link>
             <Link href="/services" className={`${styles.nav_link}`}>
-              Service
+              Services
             </Link>
             <Link href="/menus" className={`${styles.nav_link}`}>
-              Menu
+              Menus
             </Link>
+
             <div className={`${styles.dropdown}`}>
-              <Link
+              <a
                 href="#"
                 className={`${styles.nav_link} ${styles.dropdown_toggle}`}
                 data-toggle="dropdown"
               >
                 Pages
-              </Link>
-              <div className={`${styles.dropdown_menu} ${styles.text_capitalize}`}>
+              </a>
+
+              <div
+                className={`${styles.dropdown_menu} ${styles.text_capitalize}`}
+              >
                 <Link href="/reservation" className={`${styles.dropdown_item}`}>
                   Reservation
                 </Link>
@@ -58,6 +104,7 @@ export default function Navbar() {
                 </Link>
               </div>
             </div>
+
             <Link href="/contact" className={`${styles.nav_link}`}>
               Contact
             </Link>
@@ -67,3 +114,5 @@ export default function Navbar() {
     </div>
   );
 }
+
+export default Navbar;
--- pages/search/index.js
+++ pages/search/index.js
@@ -1,7 +1,10 @@
+import PageHeader from '@/Components/modules/PageHeader/PageHeader'
 import React from 'react'
 
 export default function Search() {
   return (
-    <h1>Search</h1>
+  <>
+  <PageHeader route="search"/>
+  </>
   )
 }
